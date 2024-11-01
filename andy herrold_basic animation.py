18fa664d73190ae10d587bd945b880dbaa61a771
AndyHerrold <APHERROLD@GMAIL.COM>
2024-11-01 10:26:28 -0500

    Add files via upload

--- andy herrold_basic animation.py
+++ andy herrold_basic animation.py
@@ -24,6 +24,29 @@ def main():
 # Refresh display
         screen.blit(backround, (0, 0))
         pygame.display.flip()
+#build a box
+        box = pygame.surface ((25, 25))
+        box=box.convert()
+        box.fill ((255, 0, 0))
+#set box variables
+        box_x = 0
+        box_y = 200
+# modify box values
+        box_x +=5
+#check boundaries
+        if box_x> screen.get_width():
+            box_x=0
+#refresh screeen
+        scren.blit(backround, (0, 0,))
+        screen.blit(box, (box_x, box_y))
+        pygame.display.flip()
+        
+#load an image
+        
+        
+#set image variables
+
+
         
     pygame.quit()
     
